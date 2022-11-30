8eca9af191fa14107ffb6c6840d8e50484aafbcf
kevsup <kev.supakkul@gmail.com>
2022-11-29 19:51:30 -0800

    camera photo capture script

---
+++ query_camera.py
@@ -0,0 +1,26 @@
+from blinkpy.blinkpy import Blink
+from blinkpy.auth import Auth
+from blinkpy.helpers.util import json_load
+import threading
+import time
+
+blink = Blink()
+blink.auth = Auth(json_load("credentials.json"))
+blink.start()
+
+camera = blink.cameras["Living room"]
+counter = 556
+
+def capture():
+    try:
+        camera.snap_picture()
+        time.sleep(10)
+        blink.refresh()
+        global counter
+        camera.image_to_file('photos/' + 'umi_' + str(counter) + '.jpg')
+        counter += 1
+    except:
+        pass
+    threading.Timer(50.0, capture).start()
+
+capture()
